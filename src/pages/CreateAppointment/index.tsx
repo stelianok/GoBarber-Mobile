38ea9b59dc08587ee2e66f517f966ec03f009694
Kauã Steliano <stelianok@gmail.com>
2021-01-25 14:28:10 -0300

    function to create appointments is done.

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -26,7 +26,8 @@ import {
   SectionContent,
   Hour,
   HourText,
-
+  CreateAppointmentButton,
+  CreateAppointmentButtonText,
 } from './styles';
 import api from '../../services/api';
 import { Platform } from 'react-native';
@@ -49,7 +50,7 @@ const CreateAppointment: React.FC = () => {
   const { user } = useAuth();
   const route = useRoute();
 
-  const { goBack } = useNavigation();
+  const { goBack, navigate } = useNavigation();
 
   const routeParams = route.params as RouteParams;
 
@@ -129,6 +130,25 @@ const CreateAppointment: React.FC = () => {
   const handleSelectHour = useCallback((hour: number) => {
     setSelectedHour(hour);
   }, [])
+
+  const handleCreateAppointment = useCallback(async () => {
+    try {
+      const date = new Date(selectedDate)
+
+      date.setHours(selectedHour);
+      date.setMinutes(0);
+
+      await api.post('/appointments', {
+        provider_id: selectedProvider,
+        date,
+      });
+
+      navigate('AppointmentCreated', { date: date.getTime });
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }, []);
   return (
     <Container>
       <Header>
@@ -220,10 +240,13 @@ const CreateAppointment: React.FC = () => {
                 </Hour>
               ))}
             </SectionContent>
-
           </Section>
         </Schedule>
-
+        <CreateAppointmentButton onPress={() => { handleCreateAppointment }}>
+          <CreateAppointmentButtonText>
+            Agendar
+          </CreateAppointmentButtonText>
+        </CreateAppointmentButton>
       </Content>
     </Container>
   )
